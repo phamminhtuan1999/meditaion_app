70d24be31ec19cf338b83b317d30dfdd07fe3451
tuanpm <phamminhtuan1999@gmail.com>
2022-07-04 21:40:20 +0700

    add image background and check orientation

--- meditation_millera_go_dev/lib/pages/home_page.dart
+++ meditation_millera_go_dev/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:meditation_millera_go_dev/utils/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,18 +7,60 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    final orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
       backgroundColor: kPrimaryColor,
       body: SafeArea(
-        child: Column(
-          children: const [
-            Text("Hi Jack, Welcome"),
-            Text("to Silent Mode"),
-            Text("Explore the app, Find some peace of mind to prepare for meditation."),
-          ],
-        ),
+        child: orientation == Orientation.portrait
+            ? Stack(
+                children: const [
+                  GetStartedBackground(),
+                  GetStartedHeader(),
+                ],
+              )
+            : Row(
+                children: const [
+                  Expanded(child: GetStartedBackground()),
+                  Expanded(child: GetStartedHeader())
+                ],
+              ),
       ),
     );
   }
 }
 
+class GetStartedBackground extends StatelessWidget {
+  const GetStartedBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FractionallySizedBox(
+        heightFactor: 0.6,
+        widthFactor: 1,
+        child: FittedBox(
+            fit: BoxFit.cover,
+            child: SvgPicture.asset("assets/images/bg_get_started.svg")),
+      ),
+    );
+  }
+}
+
+class GetStartedHeader extends StatelessWidget {
+  const GetStartedHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset("assets/images/ic_logo.svg"),
+        const Text("Hi Jack, Welcome"),
+        const Text("to Silent Mode"),
+        const Text(
+            "Explore the app, Find some peace of mind to prepare for meditation."),
+      ],
+    );
+  }
+}
